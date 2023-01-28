9a5235c9107282e9f98abb61f287be47a5e51c5e
vnkid93 <tran.tony7@gmail.com>
2023-01-28 15:59:05 +0100

    SetupController - added implementation of login request

--- src/main/java/com/phrase/assignment/view/SetupController.java
+++ src/main/java/com/phrase/assignment/view/SetupController.java
@@ -1,19 +1,26 @@
 package com.phrase.assignment.view;
 
 import com.phrase.assignment.service.LoginService;
+import com.phrase.assignment.service.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/setup")
 public class SetupController {
     private final LoginService loginService;
-
     public SetupController(LoginService loginService) {
         this.loginService = loginService;
     }
 
     @PostMapping
-    public String login() {
-        return null;
+    public ResponseEntity<String> login(User user) {
+        try {
+            String token = loginService.login(user);
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
